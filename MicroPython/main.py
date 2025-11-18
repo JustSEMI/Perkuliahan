63d1af59438c2f5f10d1309e6dd3d4b2d4d799e5
SEMIII <smuel.fht@gmail.com>
2025-11-18 21:59:49 +0700

    ip decoder for CTF

--- MicroPython/main.py
+++ MicroPython/main.py
@@ -1,19 +1,28 @@
 from machine import Pin, ADC
 import time
 
-ldr = ADC(0)
-led1 = Pin(15, Pin.OUT)
-led2 = Pin(13, Pin.OUT)
-led3 = Pin(12, Pin.OUT)
-led4 = Pin(14, Pin.OUT)
-led5 = Pin(16, Pin.OUT)
+#PIN SETUP
+ldr = ADC(0)    #LDR PIN
+led1 = Pin(15, Pin.OUT) #HIJAU
+led2 = Pin(13, Pin.OUT) #HIJAU
+led3 = Pin(12, Pin.OUT) #HIJAU
+led4 = Pin(14, Pin.OUT) #KUNING
+led5 = Pin(16, Pin.OUT) #MERAH
+# 15 = GPIO15 --> D8
+# 13 = GPIO13 --> D7
+# 12 = GPIO12 --> D6
+# 14 = GPIO14 --> D5
+# 16 = GPIO16 --> D0
 
+#THRESHOLDS SETUP
+#LDR Value 0 -----------------------------------------> 1024 (5V)
 T1 = 1024
 T2 = 819
 T3 = 614
 T4 = 409
 T5 = 204
 
+#ALERT FUNCTION KALAU VALUE NYENTUH DI <= T5
 def alert():
     for _ in range(1):
         led5.on()
@@ -21,24 +30,29 @@ def alert():
         led5.off()
         time.sleep_ms(200)
 
+#MAIN LOOP
 while True:
+    #VARIABEL UNTUK MEMBACA NILAI LDR
     value = ldr.read()
     
+    # MATIKAN SEMUA LED TERLEBIH DAHULU
     led1.off(); led2.off(); led3.off(); led4.off(); led5.off()
     
-    if value == T1:
+    #BARU NYALAKAN LED BERDASARKAN NILAI LDR
+    if value == T1: #<= 1024
         led1.on()
-    if value <= T2:
+    if value <= T2: #<= 819
         led2.on()
-    if value <= T3:
+    if value <= T3: #<= 614
         led3.on()
-    if value <= T4:
+    if value <= T4: #<= 409
         led4.on()
-    if value <= T5:
+    if value <= T5: #<= 204
         alert()
 
     time.sleep_ms(5)
 
+# DOCUMENTATION
 '''
 LDR Value 0 ----------------------------------------- 1024
 |-------|-------|-------|-------|--------|
---
+++ ip_decoder.py
@@ -0,0 +1,35 @@
+def decode_ip_to_ascii(data_string):
+    lines = data_string.strip().split('\n')
+    decoded_flag = ""
+    
+    for line in lines:
+        ip_part = line.split(' ')[0] 
+
+        octets = ip_part.split('.')
+        
+        if len(octets) >= 2:
+            try:
+                octet1 = int(octets[0])
+                octet2 = int(octets[1])
+                
+                char1 = chr(octet1)
+                char2 = chr(octet2)
+                
+                decoded_flag += char1 + char2
+                
+            except ValueError:
+                continue
+            except UnicodeEncodeError:
+                continue
+
+    return decoded_flag
+
+ip_data = """
+192.100.100.1
+"""
+
+final_flag = decode_ip_to_ascii(ip_data)
+
+print("=========================================")
+print(f"{final_flag}")
+print("=========================================")
